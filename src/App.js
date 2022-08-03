7b6369fdc34eaa91b0c4c7effcc39d59d81f18e7
Andriy <andygrigs88@gmail.com>
2022-08-03 14:36:21 +0300

    adding the search bar

--- src/App.js
+++ src/App.js
@@ -40,7 +40,11 @@ function App() {
 
       
      <div className="cardsTitle">
-        <h2>Репетитори</h2>
+        <h2>Всі репетитори</h2>
+        <div className="search-bar" >
+          <img src="./img/icons8-search-24.png" alt="search"/>
+          <input type="text" placeholder="Пошук..."/>
+        </div>
      </div>
 
         <div className="cardContainer">   
